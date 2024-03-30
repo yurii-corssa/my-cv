02f5cfb192d6ef7973bb24bfb4ef9f8944e3217a
yurii-corssa <yurii.corssa@gmail.com>
2024-03-30 22:31:53 +0100

    create initial data

---
+++ src/initialData.js
@@ -0,0 +1,126 @@
+export const jobTitle = 'Junior Full Stack Developer';
+
+export const techSkills = [
+  'JavaScript (ES6+)',
+  'React.js',
+  'Redux',
+  'RTK Query',
+  'HTML5',
+  'CSS3/SASS',
+  // 'Responsive/Adaptive design',
+  'RWD',
+  'Node.js',
+  'Express.js',
+  'RESTful APIs',
+  'MongoDB/Mongoose',
+  'Git/GitHub',
+  'Scrum',
+];
+
+export const softSkills = [
+  'Autonomous',
+  'Conscientious',
+  'Meticulous',
+  'Diligent',
+];
+
+export const languages = [
+  'English - Beginner',
+  'Polish - Intermediate',
+  'Ukrainian - Native',
+  'Russian - Native',
+];
+
+export const summary =
+  'Embarking on my career as a Junior Full Stack Developer, I have developed a strong skill set in both frontend and backend technologies, including HTML, CSS, JavaScript, React, Redux, and Node.js. My practical experience spans from crafting responsive UIs to implementing server-side logic, underpinned by a solid understanding of modern web development practices. Adept in collaborative team settings, I am enthusiastic about applying my skills to contribute to meaningful projects, with a keen interest in continuous learning and professional growth in the tech industry.';
+
+export const projects = [
+  {
+    name: 'Contact Keeper',
+    links: [
+      {
+        name: 'Contact Keeper',
+        link: 'https://yurii-corssa.github.io/contact-keeper-app/',
+      },
+      {
+        name: 'frontend link',
+        link: 'https://github.com/yurii-corssa/contact-keeper-app',
+      },
+      {
+        name: 'backend link',
+        link: 'https://github.com/yurii-corssa/contacts-manager-api',
+      },
+    ],
+    technologies: ['React', 'Redux', 'Node.js', 'Express.js', 'MongoDB'],
+    description:
+      'Developed a full-stack web application for managing personal contacts. Features include secure user authentication, dynamic contact management, and a responsive UI. I implemented state management using Redux, designed a user-friendly interface with Chakra UI, and developed a RESTful API backend. Gained extensive experience in full-stack development and user-centric design.',
+  },
+  {
+    name: 'Your Pet',
+    links: [
+      {
+        name: 'Your Pet',
+        link: 'https://yurii-corssa.github.io/your-pets-project/',
+      },
+      {
+        name: 'frontend link',
+        link: 'https://github.com/yurii-corssa/your-pets-project',
+      },
+      {
+        name: 'backend link',
+        link: 'https://github.com/PavelPereverzev1/yourPets-project-backend',
+      },
+    ],
+    technologies: ['React', 'Redux', 'Node.js', 'Express.js', 'MongoDB'],
+    description:
+      'Contributed as a frontend team lead in a team project, developing a comprehensive web service for pet enthusiasts. My key responsibilities included developing the user page and main sections of the notices page, along with implementing request logic for user and notice data. This fully functional application features seamless operation and an intuitive user interface.',
+  },
+  // {
+  //   name: 'WebStudio',
+  //   links: [
+  //     {
+  //       name: 'WebStudio',
+  //       link: 'https://yurii-corssa.github.io/goit-markup-hw-07/',
+  //     },
+  //     {
+  //       name: 'frontend link',
+  //       link: 'https://github.com/yurii-corssa/goit-markup-hw-07',
+  //     },
+  //   ],
+  //   technologies: ['HTML', 'CSS'],
+  //   description:
+  //     'Developed a portfolio website for a web studio, highlighting its services. As the developer, I crafted a responsive design using HTML5 and CSS3. Integrated user-friendly form for service inquiries. Acquired hands-on experience in project execution and client engagement.',
+  // },
+  {
+    name: 'Bookshelf',
+    links: [
+      {
+        name: 'Bookshelf',
+        link: 'https://pavelpereverzev1.github.io/booklovers-team/',
+      },
+      {
+        name: 'frontend link',
+        link: 'https://github.com/PavelPereverzev1/booklovers-team',
+      },
+    ],
+    technologies: ['HTML', 'SASS', 'JavaScript', 'Parsel'],
+    description:
+      'Team project to create a book search service. Role: developer. Implementation of adding/removing books to the Shopping List, implementation of registration simulation.',
+  },
+  {
+    name: 'Simply Chocolate',
+    links: [
+      {
+        name: 'Simply Chocolate',
+        link: 'https://1mposs1blee.github.io/chocolate-team-project/',
+      },
+      {
+        name: 'frontend link',
+        link: 'https://github.com/1mposs1blee/chocolate-team-project',
+      },
+    ],
+    technologies: ['HTML', 'CSS', 'JavaScript', 'Vite'],
+    description:
+      'Team project to create a landing page online chocolate store. Role: developer. Creating the sections "How Its Made", "Unique Components", as well as working with the sections "Revievs", "Our Products".',
+  },
+];
