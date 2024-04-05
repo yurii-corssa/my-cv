cfba85e035c34443c597d73c928fdaa0ce0a6844
yurii-corssa <yurii.corssa@gmail.com>
2024-04-05 10:37:16 +0200

    Create handleProjectsForm.js

---
+++ src/js/handleProjectsForm.js
@@ -0,0 +1,32 @@
+import createProject from './createProject';
+import { initialProjects } from './initialData';
+import generateProjectListMarkup from './markup/generateProjectListMarkup';
+import { getAllProjects } from './services/api';
+
+const projectSelectEl = document.querySelectorAll('.js-project-select');
+
+const projects = await getAllProjects();
+
+const handleChange = e => {
+  const project = e.currentTarget;
+  const value = project.value;
+  const position = project.classList[project.classList.length - 1];
+
+  createProject(value, position);
+};
+
+projectSelectEl.forEach((el, i) => {
+  const projectListMarkup = generateProjectListMarkup(
+    projects,
+    initialProjects[i]
+  );
+
+  el.innerHTML = projectListMarkup;
+
+  const value = el.value;
+  const position = el.classList[el.classList.length - 1];
+
+  createProject(value, position);
+
+  el.addEventListener('change', handleChange);
+});
