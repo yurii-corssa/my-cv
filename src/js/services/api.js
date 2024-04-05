25dc29611f68bfd3fc7caf9fd1cc89fb9a647312
yurii-corssa <yurii.corssa@gmail.com>
2024-04-05 10:37:42 +0200

    Update api.js

--- src/js/services/api.js
+++ src/js/services/api.js
@@ -4,9 +4,28 @@ const BASE_URL = 'https://657099a609586eff6641af70.mockapi.io/';
 
 export const getSummary = async position => {
   try {
-    const res = await axios.get(`${BASE_URL}summary?name=${position}`);
-    console.log();
-    return res.data[0].text;
+    const res = await axios.get(`${BASE_URL}portfolio/1`);
+    const { text } = res.data.summaries.find(el => el.name === position);
+    return text;
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+export const getAllProjects = async () => {
+  try {
+    const res = await axios.get(`${BASE_URL}portfolio/1`);
+    return res.data.projects;
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+export const getProject = async projectName => {
+  try {
+    const res = await axios.get(`${BASE_URL}portfolio/1`);
+    const project = res.data.projects.find(el => el.name === projectName);
+    return project;
   } catch (error) {
     alert(error.message);
   }
