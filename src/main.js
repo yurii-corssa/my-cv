bd6c675e3f19f07cbef83df37e498b48bd959ef7
yurii-corssa <yurii.corssa@gmail.com>
2024-04-05 10:37:59 +0200

    update imports

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
-import './js/listItemAdditionController';
-import './js/textAdditionController';
-import './js/projectAdditionController';
+import './js/createSkill';
+import './js/createProject';
+import './js/initialData';
+import './js/handleJobNameForm';
+import './js/handleSummaryForm';
+import './js/handleProjectsForm';
+import './js/handleSkillsForm';
+import './js/handleColorPicker';
