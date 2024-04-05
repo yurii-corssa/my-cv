0fb79d99684cf680b7a035ac0d05793a3e474ec5
yurii-corssa <yurii.corssa@gmail.com>
2024-04-05 10:37:07 +0200

    Create initialData.js

---
+++ src/js/initialData.js
@@ -0,0 +1,41 @@
+export const initialJobName = 'Junior frontend Developer';
+
+export const initialSummary = 'frontend';
+
+export const initialProjects = [
+  'Contact Keeper',
+  'Your Pet',
+  'Bookshelf',
+  'Simply Chocolate',
+  null,
+];
+
+export const initialSoftSkills = [
+  'Autonomous',
+  'Conscientious',
+  'Meticulous',
+  'Diligent',
+];
+export const initialLanguages = [
+  'English - Beginner',
+  'Polish - Intermediate',
+  'Ukrainian - Native',
+  'Russian - Native',
+];
+
+export const initialTechSkills = [
+  'JavaScript (ES6+)',
+  'React.js',
+  'Redux',
+  'RTK Query',
+  'HTML5',
+  'CSS3/SASS',
+  // 'Responsive/Adaptive design',
+  'RWD',
+  'Node.js',
+  'Express.js',
+  'RESTful APIs',
+  'MongoDB/Mongoose',
+  'Git/GitHub',
+  'Scrum',
+];
